a876c99b240e2d4cc59cbcd127bdd2873315ccc4
Tristan SARTON DU JONCHAY <tristan-sarton@hotmail.fr>
2024-03-21 15:11:56 -0400

    Added comments to code

--- pyrld/_rld.py
+++ pyrld/_rld.py
@@ -3,31 +3,49 @@ import scipy as scp
 import matplotlib.pyplot as plt
 
 class BayesRLD(object):
-    """Object to record degradation data and infer RLD.
+    """Class to record degradation data and update RLD on the fly.
     The stochastic degradation model used to compute the RLD is an increasing exponential 
-    as defined in Gebraeel et al., "Residual-life distributions from component 
-    degradation signals: a Bayesian approach" (2007).  
+    with Brownian motion error as defined in Gebraeel et al., "Residual-life distributions 
+    from component degradation signals: a Bayesian approach" (2007).  
 
-    The model implemented is that assuming Bownian motion error.
+    The degradation signal assumed in Gebraeel et al. (2007) is:
+
+        S(t) = phi + theta*exp(beta*t)*exp(err(t) - (sig**2/2)*t)
+
+    where:
+        > S(t)   : degradation signal
+        > phi    : a constant 
+        > theta  : distributed as a lognormal random variable
+        > beta   : distributed as a normal random variable
+        > err(t) : Brownian motion error with mean 0 and variance sig**2*t
+
+    Inputs:
+        > phi  : a constant in the degradation model
+        > mu0  : mean of log(theta) (prior)
+        > sig0 : standard deviation of log(theta) (prior)
+        > mu1  : mean of beta (prior) 
+        > sig1 : standard deviation of beta (prior)
+        > sig  : standard deviation of error terms (dim = 1/sqrt(min))
+        > D    : threshold for failure detection
     """
 
     def __init__(self, phi, mu0, sig0, mu1, sig1, sig, D):
-        # convert arguments into (private) attributes
-        self.phi_ = phi # constant in degradation model
-        self.mu0_ = mu0 # mean of theta (prior)
-        self.sig0_ = sig0 # standard deviation of theta (prior)
-        self.mu1_ = mu1 # mean of beta (prior)
-        self.sig1_ = sig1 # standard deviation of beta (prior)
-        self.sig_ = sig # standard deviation of error terms
-        self.D_ = D # threshold for failure detection (used to generate synthetic data)
+        # convert arguments into attributes
+        self.phi_ = phi 
+        self.mu0_ = mu0 
+        self.sig0_ = sig0 
+        self.mu1_ = mu1
+        self.sig1_ = sig1
+        self.sig_ = sig 
+        self.D_ = D
         self.logD_ = np.log(D) # logged failure threshold (used to compute RLDs)
 
-        # bivariate normal distribution parameters (mean and variance)
-        self.muT_ = None # mean of theta (posterior)
-        self.sigT_ = None # standard deviation of theta (posterior)
-        self.muB_ = None # mean of beta (posterior)
-        self.sigB_ = None # standard deviation of beta (posterior)
-        self.corr_ = None # correlation b/w beta and theta given observations
+        # bivariate normal distribution parameters (mean and variance) - updated each time new data is recorded
+        self.muT_ = None # mean of ln(theta) (posterior distribution)
+        self.sigT_ = None # standard deviation of ln(theta) (posterior distribution)
+        self.muB_ = None # mean of beta (posterior distribution)
+        self.sigB_ = None # standard deviation of beta (posterior distribution)
+        self.corr_ = None # correlation b/w beta and theta given the observations
 
         # containers for observations of signal degradation signal
         self.timestamps_ = []
@@ -37,19 +55,29 @@ class BayesRLD(object):
         return
     
     def push(self, timestamp, signal):
-        # to record real-time device data
+        """Record a new observation and automatically update RLD.
+
+        Inputs:
+            > timestamp : time at which the observation is made
+            > signal    : value of the made observation
+        """
+        # record observation
         self.timestamps_.append(timestamp)
         self.signal_.append(signal)
+        # transform observation
         self.logged_signal_.append(np.log(signal - self.phi_))
         if len(self.signal_) == 1:
             self.logged_signal_diff_.append(self.logged_signal_[0])
         else:
             self.logged_signal_diff_.append(self.logged_signal_[-1] - self.logged_signal_[-2])
-        self._update_rld()
+        # update RLD
+        self._update_posterior()
         return
     
-    def _update_rld(self):
-        # proposition 2 from Gebraeel et al. (2007)
+    def _update_posterior(self):
+        """Update joint posterior distribution of ln(theta) and beta using all the observations recorded so far.
+        Formulas taken from Proposition 2 in Gebraeel et al., "Residual-life distributions from component 
+        degradation signals: a Bayesian approach" (2007)."""
         mu1_p = self.mu1_ - (self.sig_**2/2)
         den = (self.sig0_**2 + self.sig_**2*self.timestamps_[0]) * (self.sig1_**2*self.timestamps_[-1] + self.sig_**2) \
               - self.sig0_**2*self.sig1_**2*self.timestamps_[0]
@@ -66,7 +94,13 @@ class BayesRLD(object):
         return
     
     def rld_cdf(self, t):
-        # the cumulative distribution function of the residual life distribution
+        """Compute Cumulative Distribution Function (CDF) of the Residual Life Distribution (RLD).
+        Formula taken from Gebraeel et al., "Residual-life distributions from component 
+        degradation signals: a Bayesian approach" (2007).
+        
+        Input:
+            > t : time at which to evaluate the CDF
+        """
         if len(self.signal_) == 0:
             raise Exception("Please first record a data point before calling rld_cdf().")
         mu = self.logged_signal_[-1] + self.muB_*t
@@ -75,7 +109,13 @@ class BayesRLD(object):
         return scp.stats.norm.cdf(g)
     
     def rld_pdf(self, t):
-        # the probability density function of the residual life distribution
+        """Compute Probability Density Function (PDF) of the Residual Life Distribution (RLD).
+        Formula taken from Gebraeel et al., "Residual-life distributions from component 
+        degradation signals: a Bayesian approach" (2007).
+        
+        Input:
+            > t : time at which to evaluate the PDF
+        """
         if len(self.signal_) == 0:
             raise Exception("Please first record a data point before calling rld_pdf().")
         mu = self.logged_signal_[-1] + self.muB_*t
@@ -85,6 +125,12 @@ class BayesRLD(object):
         return scp.stats.norm.pdf(g) * g_prime
     
     def plot_cdf(self, interval, coeff=5):
+        """Plot the Cumulative Distribution Function (CDF) of the Residual Life Distribution (RLD).
+        
+        Inputs:
+            > interval : time interval over which to plot the CDF
+            > coef     : coeff by which interval is scaled to get the number of points to plot
+        """
         time = np.linspace(0.1,interval,num=int(coeff*interval))
         cdf = [self.rld_cdf(t) for t in time]
         fig, ax = plt.subplots()
@@ -94,6 +140,12 @@ class BayesRLD(object):
         return fig
     
     def plot_pdf(self, interval, coeff=5):
+        """Plot the Probability Density Function (PDF) of the Residual Life Distribution (RLD).
+        
+        Inputs:
+            > interval : time interval over which to plot the PDF
+            > coef     : coeff by which interval is scaled to get the number of points to plot
+        """
         time = np.linspace(0.1,interval,num=int(coeff*interval))
         pdf = [self.rld_pdf(t) for t in time]
         fig, ax = plt.subplots()
@@ -103,6 +155,15 @@ class BayesRLD(object):
         return fig
     
     def synthetic_data(self, dt, seed=10, n_extra=20):
+        """Generate synthetic data assuming the increasing exponential model.
+        The function stops generating data points when the threshold self.D_ is reached
+        plus n_extra additional points.
+        
+        Inputs:
+            > dt      : time interval between any two observations
+            > seed    : seed for random number geenrator
+            > n_extra : number of additional points to generates
+        """
         # initialize random number generator 
         rng = np.random.Generator(np.random.MT19937(seed))
         # sample stochastic parameters theta and beta
@@ -146,6 +207,12 @@ class BayesRLD(object):
         return time, signal, actual_fail_time, fig
     
     def percentile(self, p=0.5, x0=400):
+        """ Compute the (100*p)th percentile of the Cumulative Distribution Function (CDF) of the Residual Life Distribution (RLD).
+        
+        Inputs:
+            > p  : probability, must be between 0 and 1
+            > x0 : initial value for the fsolve function
+        """
         # compute time at specified percentile given current RLD
         if len(self.signal_) == 0:
             raise Exception("Please first record a data point before calling rld_pdf().")
--- tests/main.py
+++ tests/main.py
@@ -1,3 +1,26 @@
+"""
+Example script creating an instance of the BayesRLD class with input parameters
+taken from data found in Gebraeel's PhD thesis (2003) and one of his publications (2007).
+
+The class instance then generates synthetic data using the provided input parameters
+before simulating the sampling of new observations which are used to update the 
+Residual Life Distribution (RLD) under the hood.
+
+Reference inputs:
+ > phi  = 0                : constant in degradation model                         : taken directly from [1] (p. 145)
+ > sig  = np.sqrt(0.07721) : standard deviation of error terms (dim = 1/sqrt(min)) : taken directly from [1] (p. 146)
+ > mu0  = -7.6764738       : mean of log(theta) (prior)                            : computed from Table 6.1 p.140 in [1] (cf. Excel file in 'tests' folder)
+ > sig0 = 0.56172343       : standard deviation of log(theta) (prior)              : computed from Table 6.1 p.140 in [1] (cf. Excel file in 'tests' folder)
+ > mu1  = 0.046585         : mean of beta (prior)                                  : computed from Table 6.1 p.140 in [1] (cf. Excel file in 'tests' folder)
+ > sig1 = 0.0071362        : standard deviation of beta (prior)                    : computed from Table 6.1 p.140 in [1] (cf. Excel file in 'tests' folder)
+ > D    = 0.03             : threshold for failure detection                       : taken directly from [2]
+ > dt   = 2                : data recording interval, every dt minutes             : taken directly from [1] or [2]
+
+References:
+    > [1] Gebraeel's PhD thesis, "Real-time degradation modeling an residual life prediction for component maintenance and replacement" (2003)
+    > [2] Gebraeel et al., "Residual-life distributions from component degradation signals: a Bayesian approach" (2007)
+"""
+
 import os
 import sys
 fpath = os.path.join(os.pardir, "")
@@ -8,44 +31,43 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
-    # input degradation model parameters (prior) - take from Gebraeel's PhD thesis (2003)
-    phi = 0 # constant in degradation model - from p. 145 of Gebraeel's PhD thesis (2003)
-    sig = np.sqrt(0.07721) # standard deviation of error terms (dim = 1/minute**.5) - p/. 146 from Gebraeel's PhD thesis (2003)
-    mu0 = -7.6764738 # mean of log(theta) (prior) - from Table 6.1 p.140 in Gebraeel's PhD thesis (2003)
-    sig0 = 0.56172343 # standard deviation of log(theta) (prior) - from Table 6.1 p.140 in Gebraeel's PhD thesis (2003)
-    mu1 = 0.046585 # mean of beta (prior) - from Table 6.1 p.140 in Gebraeel's PhD thesis (2003)
-    sig1 = 0.0071362 # standard deviation of beta (prior) - from Table 6.1 p.140 in Gebraeel's PhD thesis (2003)
-    D = 0.03 # threshold for failure detection - from Gebraeel et al. (2007)
-    dt = 2 # data recording interval, every dt minutes
+    # input parameters
+    phi = 0
+    sig = np.sqrt(0.07721)
+    mu0 = -7.6764738
+    sig0 = 0.56172343
+    mu1 = 0.046585 
+    sig1 = 0.0071362
+    D = 0.03
+    dt = 2
+    interval = 3000 # interval over which to plot cdf and pdf [min]
 
-    # create RLD object
+    # create BayesRLD object
     rld = BayesRLD(phi, mu0, sig0, mu1, sig1, sig, D)
 
     # generate synthetic data
     time, signal, actual_fail_time, fig = rld.synthetic_data(dt, seed=0, n_extra=5)
     n = len(time)
 
-    # simulate real-time degradation signal recording
-    # first record and plot cdf and pdf
-    rld.push(time[0], signal[0])
-    interval = 3000 # interval over which to plot cdf and pdf [min]
+    # simulation
+    # first data record and RLD update
+    rld.push(time[0], signal[0]) 
     rld.plot_cdf(interval)
     rld.plot_pdf(interval)
-    t_10 = rld.percentile(p=0.1)
-    t_50 = rld.percentile(p=0.5)
-    t_90 = rld.percentile(p=0.9)
+    t_10 = rld.percentile(p=0.1) # 10% chance the device fails before t_10 (10th percentile)
+    t_50 = rld.percentile(p=0.5) # 50% chance the device fails before t_50 (median)
+    t_90 = rld.percentile(p=0.9) # 90% chance the device fails before t_90 (90th percentile)
     print(f"t = {time[0]}")
     print(f"   [10%, 90%] : [{time[0]+round(t_10,3)}, {time[0]+round(t_90,3)}]")
     print(f"   med : {time[0]+round(t_50, 3)}\n")
 
-    # a few more records...
+    # a few more records and RLD updates...
     N = int(n/2)
     for i in range(1, N):
         rld.push(time[i], signal[i])
 
-    # ... one more record with plots
+    # ... one more record and RLD update
     rld.push(time[N], signal[N])
-    interval = 3000 # interval over which to plot cdf and pdf [min]
     rld.plot_cdf(interval)
     rld.plot_pdf(interval)
     t_10 = rld.percentile(p=0.1)
@@ -55,14 +77,13 @@ if __name__ == "__main__":
     print(f"   [10%, 90%] : [{time[N]+round(t_10,3)}, {time[N]+round(t_90,3)}]")
     print(f"   med : {time[N]+round(t_50, 3)}\n")
 
-    # a few more records...
+    # a few more records with RLD updates...
     N2 = int(n/4)
     for i in range(N+1, N+N2):
         rld.push(time[i], signal[i])
 
-    # ... one more record with plots
+    # ... one more record RLD updates
     rld.push(time[N+N2], signal[N+N2])
-    interval = 3000 # interval over which to plot cdf and pdf [min]
     rld.plot_cdf(interval)
     rld.plot_pdf(interval)
     t_10 = rld.percentile(p=0.1)
